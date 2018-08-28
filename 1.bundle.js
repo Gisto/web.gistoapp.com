5ca4353cb3571a64efa1c52535c72a8fa12aca14
Sasha Khamkov <sanusart@gmail.com>
2018-08-28 21:52:19 +0300

    initial

---
+++ 1.bundle.js
@@ -0,0 +1,10 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{4:
+/*!************************!*\
+  !*** buffer (ignored) ***!
+  \************************/
+/*! no static exports found */function(n,o){},5:
+/*!************************!*\
+  !*** crypto (ignored) ***!
+  \************************/
+/*! no static exports found */function(n,o){}}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIxLmJ1bmRsZS5qcyIsInNvdXJjZVJvb3QiOiIifQ==
